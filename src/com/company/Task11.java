89d0d966a5c39e5edbcb64f6ed073d23d7057e5b
anny-di9 <66335865+anny-di9@users.noreply.github.com>
2021-04-22 12:46:23 +0300

    11 задача исправлена

--- src/com/company/Task11.java
+++ src/com/company/Task11.java
@@ -6,14 +6,27 @@ public class Task11 {
 
   public static void main(String[] args) {
 
-    //для ввода чисел типа double использовать запятую
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Введите 1-е  число");
-    String str = Double.toString(sc.nextDouble());
-    System.out.println("Введите 2-e  число");
-    int value = (int) sc.nextDouble();
-    System.out.println("MAX число " + Math.max(Double.valueOf(str), Double.valueOf(value)));
-    System.out.println("MIN число " + Math.min(Double.valueOf(str), Double.valueOf(value)));
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Введите 1-е число :");
+    double num1 = scanner.nextDouble();
+    String convert1 = String.valueOf(num1);
+    Scanner scanner2 = new Scanner(System.in);
+    System.out.println("Введите 2-е число :");
+    double num2 = scanner2.nextDouble();
+    int convert2 = (int) num2;
+
+    double result = Math.max(num1, num2);
+    System.out.println("Сравниваемые числа : " + num1 + " и " + num2);
+    double convert3 = Double.valueOf(convert1);
+    int convert4 = (int) convert3;
+    int result2 = Math.min(convert2, convert4);
+    double result3 = (double) result2;
+    if (num1 == num2) {
+      System.out.print("Числа равны ");
+    } else {
+      System.out.println("Большее число = " + result);
+      System.out.println("Меньшее число = " + result3);
+    }
   }
 }
 
